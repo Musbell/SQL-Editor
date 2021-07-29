6d10431b9eb5b4fbc3c584e1a4bb3ecfdb74ae19
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2021-07-29 07:57:15 +0100

    feat(multi-pane): add draggable multipane for sql and result pane

--- pages/index.vue
+++ pages/index.vue
@@ -24,30 +24,61 @@
         </v-col>
       </v-row>
     </v-col>
-    <v-col cols="12" style="margin-top: -40px">
-      <textarea v-model="content" id="editor"></textarea>
+    <v-col cols="12" style="margin-top: -30px">
+      <v-divider class="mb-3"/>
+      <multipane class="foo" layout="horizontal">
+        <textarea v-model="content" id="editor"></textarea>
+        <multipane-resizer></multipane-resizer>
+        <Table/>
+      </multipane>
     </v-col>
   </v-row>
 </template>
 <script>
+import { Multipane, MultipaneResizer } from 'vue-multipane';
+import Table from "../components/Table";
 let CodeMirror;
 if(process.client) {
   CodeMirror = require('codemirror')
   require('codemirror/lib/codemirror.css')
   require('codemirror/mode/sql/sql')
   require('codemirror/theme/neo.css')
+  require('codemirror/addon/selection/active-line')
 }
 export default {
   name: 'Index',
+  components: {
+    Table,
+    Multipane,
+    MultipaneResizer
+  },
   data: () => ({
-    content: 'let a = 0'
+    content: 'select * from  users'
   }),
   mounted() {
     CodeMirror.fromTextArea(document.getElementById('editor'), {
       lineNumbers: true,
       theme: 'neo',
-      mode: 'sql'
+      mode: 'sql',
+      lineWrapping: true,
+      styleActiveLine: { nonEmpty: true },
+      styleActiveSelected: true,
     })
   }
 }
 </script>
+<style scoped>
+.multipane.foo.layout-h .multipane-resizer {
+  margin: 0; top: 0; /* reset default styling */
+  height: 5px;
+  border-radius: 5px;
+  background: #EEE;
+}
+.multipane.foo.layout-v .multipane-resizer {
+  margin: 3px; left: 3px; /* reset default styling */
+  width: 5px;
+  height: 500px;
+  border-radius: 5px;
+  background: #EEE;
+}
+</style>
