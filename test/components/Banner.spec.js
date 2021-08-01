50c9b19633ca4991e1cee9c8f7796d1a3f9218a8
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2021-08-01 21:02:09 +0100

    test(ui components): write test for UI components

--- test/components/Banner.spec.js
+++ test/components/Banner.spec.js
@@ -2,8 +2,21 @@ import { mount } from '@vue/test-utils'
 import Banner from '@/components/Banner.vue'
 
 describe('Banner', () => {
+  const state = {
+    showResult: false,
+    selectedQuery: {
+      id: null,
+      name: '',
+      query: '',
+      queryResult: []
+    }
+  }
   test('is a Vue instance', () => {
-    const wrapper = mount(Banner)
+    const wrapper = mount(Banner, {
+      computed: {
+        ...state
+      }
+    })
     expect(wrapper.vm).toBeTruthy()
   })
 })
--- test/components/HeaderQueryPanel.spec.js
+++ test/components/HeaderQueryPanel.spec.js
@@ -2,8 +2,22 @@ import { mount } from '@vue/test-utils'
 import HeaderQueryPanel from '@/components/HeaderQueryPanel.vue'
 
 describe('HeaderQueryPanel', () => {
+  const state = {
+    showResult: false,
+    selectedQuery: {
+      id: null,
+      name: '',
+      query: '',
+      queryResult: []
+    }
+  }
   test('is a Vue instance', () => {
-    const wrapper = mount(HeaderQueryPanel)
+    const wrapper = mount(HeaderQueryPanel, {
+      computed: {
+        ...state,
+        selectedQueryName: ''
+      }
+    })
     expect(wrapper.vm).toBeTruthy()
   })
 })
